0e973bfddf2b3da82cc9f41594d5e664fe943071
André Bedran <bedran.fleck@gmail.com>
2017-09-11 10:54:56 -0300

    Adicionando representação da nave.
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>

--- Modules/Player.hs
+++ Modules/Player.hs
@@ -5,3 +5,19 @@ import Modules.Render (LineRenderable(..))
 
 -- estrutura de dados que armazena o estado atual do jogador.
 data Player = Player {playerPos :: Vec2}
+
+-- instância da nave do jogador
+instance LineRenderable Player where
+    lineSegments (Player p) = map (translateLine p) $ shipLines
+
+-- constante de tamanho da nave
+shipSize = 12.0 :: Float
+
+-- Lista de linhas que compõem a nave do jogador
+shipLines :: [LineSegment]
+shipLines = pointsToSegments points
+    where points = [polar shipSize  0,-- ponto superior
+                    polar shipSize  (0.7*pi),-- ponto lateral
+                    polar (shipSize*0.2) pi,--ponto central inferior
+                    polar shipSize (1.3*pi),-- ponto lateral
+                    polar shipSize  0]-- ponto superior de fechamento da forma
