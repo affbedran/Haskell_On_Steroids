5a5a2bd7820ac580f0a597e446ac4e776f2a3397
André Bedran <bedran.fleck@gmail.com>
2017-09-11 11:05:48 -0300

    Criando módulo de Game State e funções de renderização dos objetos do jogo.
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>

---
+++ Modules/State.hs
@@ -0,0 +1,26 @@
+module Modules.State (GameState(..), initialGameState,) where
+
+import Modules.Player
+import Modules.Geometry
+import Modules.Render (LineRenderable(..))
+
+-- Estrutura de dados que mantém o Estado de Jogo
+data GameState = GameState {statePlayer :: Player}
+
+-- Instancia de GameState que renderiza os objetos do jogo.
+instance LineRenderable GameState where
+    lineSegments = stateLines
+
+-- gerando o estado inicial de jogo
+initialGameState :: GameState
+initialGameState = GameState {
+    statePlayer = initialPlayerState
+    }
+
+-- Estado inicial da nave do jogador, no centro da tela
+initialPlayerState :: Player
+initialPlayerState = Player (400, 300)
+
+-- Lista de todas as linhas que o GameState irá renderizar
+stateLines :: GameState -> [LineSegment]
+stateLines = lineSegments . statePlayer
