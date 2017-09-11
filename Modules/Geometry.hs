c0044cf450df3e38f8b829858bace8575fadba8c
André Bedran <bedran.fleck@gmail.com>
2017-09-11 11:48:57 -0300

    Arrumando indentação do projeto.
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>

--- Modules/Geometry.hs
+++ Modules/Geometry.hs
@@ -1,17 +1,15 @@
 module Modules.Geometry where
 
-    --tipo para vetores
-    type VecVal = Float
+--tipo para vetores
+type VecVal = Float
 
-    --tipo para vetor 2D
-    type Vec2 = (VecVal, VecVal)
+--tipo para vetor 2D
+type Vec2 = (VecVal, VecVal)
 
-    newtype LineSegment = LineSegment (Vec2, Vec2)
+newtype LineSegment = LineSegment (Vec2, Vec2)
 
 -- Função que converte de coordenada polar para coordenada cartesiana.
-polar :: VecVal     -- Coordenada polar radial
-         -> VecVal  -- Coordenada polar angular
-         -> Vec2    -- Coordenadas Cartesianas
+polar :: VecVal -> VecVal -> Vec2
 polar m a = (m * sin a, m * (-cos a))
 
 -- Define uma forma renderizável como uma coleção de pontos,
