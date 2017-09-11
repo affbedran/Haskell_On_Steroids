33360986aabbc1b0ba07160ae5d18dedfe5b9604
André Bedran <bedran.fleck@gmail.com>
2017-09-11 10:19:47 -0300

    Adicionando renderizadores de linha ao modulo de renderização
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>

--- Modules/Render.hs
+++ Modules/Render.hs
@@ -14,3 +14,13 @@ renderLines :: [LineSegment] -> IO()
 renderLines lns = do
     currentColor $= Color4 0.9 0.9 0.9 1.0
     renderPrimitive Lines $ mapM_ lineVertices lns
+
+-- gerando os vértices de um segmento no OpenGL
+lineVertices :: LineSegment -> IO ()
+lineVertices (LineSegment (p,p')) = do
+    ptVertex p
+    ptVertex p'
+
+-- | gerando um vértice OpenGL a partir de um único ponto.
+ptVertex :: Vec2 -> IO ()
+ptVertex = vertex . uncurry Vertex2
