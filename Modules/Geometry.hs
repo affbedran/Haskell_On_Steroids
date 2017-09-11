af6292710d284891e0d50a26a2c8d7c28305771d
André Bedran <bedran.fleck@gmail.com>
2017-09-11 10:43:52 -0300

    Criando funções de translado
    Signed-off-by: André Bedran <bedran.fleck@gmail.com>

--- Modules/Geometry.hs
+++ Modules/Geometry.hs
@@ -13,3 +13,19 @@ polar :: VecVal     -- Coordenada polar radial
          -> VecVal  -- Coordenada polar angular
          -> Vec2    -- Coordenadas Cartesianas
 polar m a = (m * sin a, m * (-cos a))
+
+-- Define uma forma renderizável como uma coleção de pontos,
+-- partindo da premissa que ela é formada por segmentos de linha,
+-- e que linhas são formadas por pontos.
+pointsToSegments :: [Vec2] -> [LineSegment]
+pointsToSegments (p:p':[])     = [LineSegment (p,p')]
+pointsToSegments (p:t@(p':ps)) = (LineSegment (p,p')) : pointsToSegments t
+
+-- Função que translada um ponto
+translate :: Vec2 -> Vec2 -> Vec2
+translate (x,y) (x', y') = (x+x', y+y')
+
+-- Função que translada uma linha com base no translado de pontos
+translateLine :: Vec2 -> LineSegment -> LineSegment
+translateLine p (LineSegment(l,l')) = LineSegment (t l , t l')
+    where t = translate p
